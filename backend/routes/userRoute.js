ef176a02479b6ba26af47bef97f2c3c0a9e13abe
Chandrashekhar Patil <balajibalajipatil4@gmail.com>
2026-02-17 08:37:00 +0530

    Payment Integration module build successfully

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser,loginUser, getProfile,updateProfile,bookAppointment, listAppointment,cancelAppointment } from '../controllers/userController.js'
+import { registerUser,loginUser, getProfile,updateProfile,bookAppointment, listAppointment,cancelAppointment,paymentRazorpay } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 
@@ -18,4 +18,5 @@ userRouter.post(
 userRouter.post('/book-appointment',authUser,bookAppointment)
 userRouter.get('/appointments',authUser,listAppointment)
 userRouter.post('/cancel-appointment',authUser,cancelAppointment)
+userRouter.post('/payment-razorpay',authUser,paymentRazorpay)
 export default userRouter
